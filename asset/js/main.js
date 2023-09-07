d047618109110d28c849a1386369d08d5522a964
danielplebani <danielpleba03@gmail.com>
2023-09-07 14:11:14 +0200

    modified array with objects

--- asset/js/main.js
+++ asset/js/main.js
@@ -2,24 +2,22 @@
 Riprendiamo il live coding visto in classe un pó di tempo fá sul carosello di immagini e rifacciamolo, questa volta usando gli oggetti.
 */
 
-/* 
-
 /* Define the slides list */
 const slides = [
     {
-        foto1 : './asset/img/01.webp'
+        foto : '01.webp'
     },
     {
-        foto2 : './asset/img/02.webp'
+        foto : '02.webp'
     },
     {
-        foto3 : './asset/img/03.webp'
+        foto : '03.webp'
     },
     {
-        foto4 : './asset/img/04.webp'
+        foto : '04.webp'
     },
     {
-        foto5 : './asset/img/05.webp'
+        foto : '05.webp'
     }
   ]
   
@@ -36,37 +34,19 @@ const slides = [
     const slidePath = slides[i];
     
     // for each slide we create the markup
-    const slideMarkup = `<img class="${activeSlide === i ? 'active' : '' }" src="${slidePath}" alt="">`
+    const slideMarkup = `<img class="${activeSlide === i ? 'active' : '' }" src="/asset/img/${slidePath.foto}">`
     //console.log(slideMarkup);
   
     sliderImagesEl.insertAdjacentHTML('beforeend', slideMarkup)
   
   }
   
-  /* 
-  MILESTONE 3
-  Al click dell'utente sulle frecce, il programma cambierà l’immagine attiva, che quindi verrà visualizzata al posto della precedente.
-  */
-  
   const slidesImages = document.querySelectorAll('.slider .images > img')  
-  
-  /* 
-  BONUS 1:
-  Aggiungere il ciclo infinito del carosello. Ovvero se è attiva la prima immagine e l'utente clicca la freccia per andare all’immagine precedente, dovrà comparire l’ultima immagine dell’array e viceversa.
-  */
-  
-  /* 
-  BONUS 2:
-  Aggiungere la visualizzazione di tutte le thumbnails sulla destra dell’immagine grande attiva, 
-  come nello screenshot proposto. Tutte le miniature avranno un layer di opacità scura, tranne quella corrispondente all’immagine attiva, che invece avrà un bordo colorato. 
-  Al click delle frecce, oltre al cambio di immagine attiva, gestire il cambio di miniatura attiva.
-  */
-
   const thumbsElement = document.querySelector('.thumbnails')
   
   for (let i = 0; i < slides.length; i++) {
     const thumbPath = slides[i];
-    const thumbMarkup = `<img class="thumb ${activeSlide === i ? 'active' : ''}" src="${thumbPath}" alt="">`
+    const thumbMarkup = `<img class="thumb ${activeSlide === i ? 'active' : ''}" src="/asset/img/${thumbPath.foto}">`
   
     thumbsElement.insertAdjacentHTML('beforeend', thumbMarkup)
     
@@ -109,7 +89,6 @@ const slides = [
   })
   
   // intercept click on the prev icon
-  // activeSlide = 0
   prevEl.addEventListener('click', function () {
   
     // select the current slide
